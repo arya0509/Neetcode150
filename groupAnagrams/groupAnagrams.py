9048924b56331474a84f5da69585e73f8011ea1a
arya0509 <151792793+arya0509@users.noreply.github.com>
2026-01-13 04:54:35 -0700

    Completed groupedAnagrams

---
+++ groupAnagrams/groupAnagrams.py
@@ -0,0 +1,18 @@
+from collections import defaultdict
+class Solution(object):
+    def groupAnagrams(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: List[List[str]]
+        """
+
+        output=defaultdict(list)
+        for i in strs:
+            key="".join(sorted(i))
+            output[key].append(i)
+        return output.values()
+
+            
+
+sol= Solution()
+print (sol.groupAnagrams(["eat","tea","tan","ate","nat","bat"]))
