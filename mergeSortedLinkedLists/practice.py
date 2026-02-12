aeba513a5c7ca3b1fbc7cd112b12566fc14fbf41
arya0509 <151792793+arya0509@users.noreply.github.com>
2026-02-12 02:26:45 -0700

    Completed two practice

---
+++ mergeSortedLinkedLists/practice.py
@@ -0,0 +1,28 @@
+# Definition for singly-linked list.
+class ListNode(object):
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+class Solution(object):
+    def mergeTwoLists(self, list1, list2):
+        """
+        :type list1: Optional[ListNode]
+        :type list2: Optional[ListNode]
+        :rtype: Optional[ListNode]
+        """
+        dummy=curr=ListNode()
+
+        while(list1 and list2):
+            if(list1.val<=list2.val):
+                curr.next=list1
+                curr=curr.next
+                list1=list1.next
+                continue
+            curr.next=list2
+            curr=curr.next
+            list2=list2.next
+        
+        if(not list1 or not list2):
+            curr.next=list1 if(not list2) else list2
+        
+        return dummy.next
